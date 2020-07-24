4b00883a274c592a5810cbaaa248ddda47c39c8f
FarDean <sfh1212@gmail.com>
2020-07-24 12:06:17 +0430

    correctd the bug

--- app.js
+++ app.js
@@ -5,10 +5,11 @@ const links = document.querySelectorAll("a");
 filterInput.addEventListener("keyup",filterNames);
 function filterNames(){
     let value = filterInput.value.toUpperCase();
-    console.log(value)
     links.forEach(a=>{
         if(a.textContent.toUpperCase().indexOf(value) === -1){
             a.parentElement.style.display= "none"
+        }else {
+            a.parentElement.style.display= ""
         }
     })
 }
